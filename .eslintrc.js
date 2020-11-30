02afff4f6c6912a0670fa626ab9f762d9cb7f859
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2020-12-01 02:09:42 +0530

    fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  rules: {
+    'no-extra-boolean-cast': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'no-prototype-builtins': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+  },
 };
--- src/component.ts
+++ src/component.ts
@@ -3,7 +3,5 @@ import {Component, ProviderMap} from '@loopback/core';
 export class AuditLogComponent implements Component {
   constructor() {}
 
-  providers?: ProviderMap = {
-  };
-
+  providers?: ProviderMap = {};
 }
