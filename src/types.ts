f388bc1e977021827b93fd9606dfcec8a7fd0ccc
mlakdawala-sf <73096294+mlakdawala-sf@users.noreply.github.com>
2021-03-16 19:16:09 +0530

    added audit creation optional flag

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import {AuditLogRepository} from './repositories';
 
 export const AuditDbSourceName = 'AuditDB';
-
+import {Options} from '@loopback/repository';
 export interface IAuditMixin<UserID> {
   getAuditLogRepository: () => Promise<AuditLogRepository>;
   getCurrentUser?: () => Promise<{id?: UserID}>;
@@ -12,3 +12,7 @@ export interface IAuditMixinOptions {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }
+interface AuditLogOption {
+  noAuditCreation: boolean;
+}
+export declare type AuditOptions = Options & AuditLogOption;
