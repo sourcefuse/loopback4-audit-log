661372d9b683bc0bff4e503540e3f2bcaced4c81
yeshamavani <83634146+yeshamavani@users.noreply.github.com>
2025-04-21 09:41:36 +0530

    fix(core): make model and respository customizable (#124)
    * fix(core): make model and resposiory customizable
    
    now user can use the default as well as their custom respository
    
    GH-121
    
    * fix(core): readme in docs folder also updated
    
    both the readme have to be in sync
    
    GH-121
    
    * feat(mixin): make model and repository customizable
    
    update the docs
    
    GH-121
    
    ---------
    
    Co-authored-by: Abir Ganguly <abir.ganguly@sourcefuse.com>

--- src/__tests__/acceptance/fixtures/repositories/audit.repository.ts
+++ src/__tests__/acceptance/fixtures/repositories/audit.repository.ts
@@ -1,18 +1,13 @@
-import {DefaultCrudRepository, Entity, juggler} from '@loopback/repository';
+import {DefaultCrudRepository, juggler} from '@loopback/repository';
 import {TestAuditLog} from '../models/audit.model';
-import {tenantGuard} from '@sourceloop/core';
 import {inject} from '@loopback/core';
 
-@tenantGuard()
 export class TestAuditLogRepository extends DefaultCrudRepository<
   TestAuditLog,
-  typeof TestAuditLog.prototype.id
+  typeof TestAuditLog.prototype.id,
+  {}
 > {
-  constructor(
-    @inject('datasources.AuditDB') dataSource: juggler.DataSource,
-    @inject('models.AuditLog')
-    private readonly auditLog: typeof Entity & {prototype: TestAuditLog},
-  ) {
-    super(auditLog, dataSource);
+  constructor(@inject('datasources.AuditDB') dataSource: juggler.DataSource) {
+    super(TestAuditLog, dataSource);
   }
 }
--- src/repositories/audit.repository.ts
+++ src/repositories/audit.repository.ts
@@ -1,19 +1,15 @@
 import {inject} from '@loopback/core';
 import {juggler, DefaultCrudRepository, Entity} from '@loopback/repository';
-
-import {AuditLog} from '../models/tenant-support';
+import {AuditLog} from '../models';
 import {AuditDbSourceName} from '../types';
-import {tenantGuard} from '@sourceloop/core';
 
-@tenantGuard()
-export class AuditLogRepository extends DefaultCrudRepository<
-  AuditLog,
-  typeof AuditLog.prototype.id
-> {
+export class AuditLogRepository<
+  LogModel extends AuditLog = AuditLog,
+> extends DefaultCrudRepository<LogModel, typeof AuditLog.prototype.id> {
   constructor(
     @inject(`datasources.${AuditDbSourceName}`) dataSource: juggler.DataSource,
     @inject('models.AuditLog')
-    private readonly auditLog: typeof Entity & {prototype: AuditLog},
+    auditLog: typeof Entity & {prototype: LogModel},
   ) {
     super(auditLog, dataSource);
   }
